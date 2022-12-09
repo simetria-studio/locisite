dd1f1382265190ac0c599ac33cbcace60e2fce7e
Simetria Studio <github@simetria.studio>
2022-12-09 18:55:56 -0300

    pdf att

--- resources/views/pages/formularios.blade.php
+++ resources/views/pages/formularios.blade.php
@@ -18,7 +18,8 @@
                     <h5>Requisição de Exame para Exame Diagnóstico de Mormo</h5>
                 </div>
                 <div class="card-footer">
-                    <a target="_blank" href="https://www.locilab.com.br/_files/ugd/8a64e2_01619f63f6704ffaa3cedd3b77a7750b.pdf" class="btn btn-secondary">Baixe aqui</a>
+                    <a target="_blank" href="{{ url('pdf/requisicao_de_exame_para_diagnostico_de_mormo.pdf') }}"
+                        class="btn btn-secondary">Baixe aqui</a>
                 </div>
             </div>
             <div class="exame3  exames card">
@@ -26,7 +27,9 @@
                     <h5>Requisição de Exame para Diagnóstico de Anemia Infecciosa Equina</h5>
                 </div>
                 <div class="card-footer">
-                    <a target="_blank" href="https://www.locilab.com.br/_files/ugd/8a64e2_7d58465a4845477b9df61d4babf7b693.pdf" class="btn btn-secondary">Baixe aqui</a>
+                    <a target="_blank"
+                        href="{{ url('pdf/requisicao_de_exame_para_diagnostico_anemia_infecciosa_equina.pdf') }}"
+                        class="btn btn-secondary">Baixe aqui</a>
                 </div>
             </div>
             <div class="exame4  exames card">
@@ -34,7 +37,9 @@
                     <h5>Tarjeta de Identificação de Contraprova</h5>
                 </div>
                 <div class="card-footer">
-                    <a target="_blank" href="https://www.locilab.com.br/_files/ugd/8a64e2_57d26fc40fad4751b9481f38a6b411f2.pdf" class="btn btn-secondary">Baixe aqui</a>
+                    <a target="_blank"
+                        href="https://www.locilab.com.br/_files/ugd/8a64e2_57d26fc40fad4751b9481f38a6b411f2.pdf"
+                        class="btn btn-secondary">Baixe aqui</a>
                 </div>
             </div>
             <div class="exame5  exames card">
@@ -42,7 +47,8 @@
                     <h5>Requisição de Teste de Homozigose Tobiana</h5>
                 </div>
                 <div class="card-footer">
-                    <a href="https://4a4c1db8-3175-43c2-933e-2563969564b1.filesusr.com/ugd/8a64e2_4e381c6227aa464581a5d6b45a3d0e57.xlsx?dn=FOR.ATN.17%20v.4%20-%20Requisi%C3%A7%C3%A3o%20de%20Exame%20de%20Homozigose%20Tobiana.xlsx" class="btn btn-secondary">Baixe aqui</a>
+                    <a href="https://4a4c1db8-3175-43c2-933e-2563969564b1.filesusr.com/ugd/8a64e2_4e381c6227aa464581a5d6b45a3d0e57.xlsx?dn=FOR.ATN.17%20v.4%20-%20Requisi%C3%A7%C3%A3o%20de%20Exame%20de%20Homozigose%20Tobiana.xlsx"
+                        class="btn btn-secondary">Baixe aqui</a>
                 </div>
             </div>
             <div class="exame6  exames card">
@@ -50,7 +56,8 @@
                     <h5>Requisição de Exame de Beta Caseína</h5>
                 </div>
                 <div class="card-footer">
-                    <a href="https://4a4c1db8-3175-43c2-933e-2563969564b1.filesusr.com/ugd/8a64e2_8dc7862be4b54d68a794532956cc6ccb.xlsx?dn=FOR.ATN.19%20v.2%20-%20Requisi%C3%A7%C3%A3o%20de%20Exame%20de%20Beta%20Case%C3%ADna.xlsx" class="btn btn-secondary">Baixe aqui</a>
+                    <a href="https://4a4c1db8-3175-43c2-933e-2563969564b1.filesusr.com/ugd/8a64e2_8dc7862be4b54d68a794532956cc6ccb.xlsx?dn=FOR.ATN.19%20v.2%20-%20Requisi%C3%A7%C3%A3o%20de%20Exame%20de%20Beta%20Case%C3%ADna.xlsx"
+                        class="btn btn-secondary">Baixe aqui</a>
                 </div>
             </div>
         </div>
